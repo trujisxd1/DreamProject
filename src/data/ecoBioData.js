5757aeb3dbf85a38b4dfc9477e3c2d5757e10596
Gustavo Martinez <gutamvol262@gmail.com>
2024-03-12 11:25:11 -0600

    contiene la data de los cards de ecoturismo

---
+++ src/data/ecoBioData.js
@@ -0,0 +1,39 @@
+export const ecoBioData = [
+    {
+      title: "Reserva de la Biosfera Sian Ka'an, Quintana Roo",
+      image: "/public/img/eco-bio-sian.jpg",
+      description: "Un paraíso natural en la costa caribeña de México. Este destino de ecoturismo ofrece una amplia variedad de ecosistemas, incluyendo selvas, manglares, lagunas y arrecifes de coral, disfruta de actividades como observación de aves, snorkel, kayak y paseos en lancha mientras te sumerges en la biodiversidad de este lugar único.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Reserva de la Biosfera El Triunfo, Chiapas",
+      image: "/public/img/eco-bio-triunfo.jpg",
+      description: "Una de las áreas protegidas más importantes de México. Hogar de una gran diversidad de flora y fauna, este destino ofrece oportunidades para practicar senderismo, observación de aves y exploración de bosques nubosos, sumérgete en la naturaleza virgen y la tranquilidad de este impresionante lugar.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Reserva de la Biosfera Calakmul, Campeche",
+      image: "/public/img/eco-bio-calakmul.jpg",
+      description: "Un tesoro oculto en la selva de Campeche, este destino de ecoturismo te invita a explorar antiguas ruinas mayas, observar jaguares y monos aulladores, y descubrir la riqueza de la selva tropical, con actividades como senderismo, avistamiento de fauna y visitas a sitios arqueológicos, Calakmul ofrece una experiencia inolvidable en contacto con la naturaleza.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Reserva de la Biosfera Ría Celestún, Yucatán",
+      image: "/public/img/eco-bio-ria.jpeg",
+      description: "Un santuario de vida silvestre en la costa de Yucatán, conocido por sus flamencos rosados, manglares y playas vírgenes, este destino ofrece oportunidades para practicar observación de aves, kayak y natación en aguas cristalinas, sumérgete en la serenidad de este paraíso natural y descubre la magia de la vida silvestre.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Reserva de la Biosfera Sierra Gorda, Querétaro",
+      image: "/public/img/eco-bio-sierra.jpeg",
+      description: "Una joya escondida en el corazón de México, este destino de ecoturismo ofrece una gran diversidad de paisajes, desde bosques de pino hasta cañones y cascadas impresionantes, disfruta de actividades como senderismo, rappel, observación de aves y visita a comunidades indígenas mientras te sumerges en la belleza y la cultura de la Sierra Gorda.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Reserva de la Biósfera Tehuacán-Cuicatlán, Puebla y Oaxaca",
+      image: "/public/img/eco-bio-tehuacan.jpg",
+      description: "Un desierto semiárido que alberga una increíble diversidad de flora y fauna, hogar de cactus gigantes, aves endémicas y pinturas rupestres prehispánicas, este destino ofrece oportunidades únicas para explorar la naturaleza y la historia de México, sumérgete en paisajes impresionantes y descubre la magia del desierto en este fascinante lugar.",
+      link: "https://www.DreamScape-Detalles.com"
+    }
+  ];
+  
