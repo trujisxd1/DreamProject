2e0b263629560d8e091b4b9580eb59ad98489a89
Gustavo Martinez <gutamvol262@gmail.com>
2024-03-12 11:03:59 -0600

    se agrego un js con los datos del festival

---
+++ src/data/festivalData.js
@@ -0,0 +1,38 @@
+export const festivalsData = [
+    {
+      title: "Festival Internacional Cervantino, Guanajuato",
+      image: "/public/img/fest-int-cervantino.jpg",
+      description: "Uno de los eventos culturales más importantes de América Latina, durante tres semanas, Guanajuato se convierte en un escenario de música, teatro, danza y artes visuales de todo el mundo. Celebra la creatividad y la diversidad en este emocionante festival.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Festival Internacional de Cine Guadalajara, Jalisco",
+      image: "/public/img/fest-giff.jpeg",
+      description: "Este evento reúne a cineastas, actores y amantes del cine de todo el mundo para celebrar la excelencia cinematográfica y promover el intercambio cultural, sumérgete en proyecciones, mesas redondas y talleres que hacen de este festival una experiencia única.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Festival Vive Latino, Ciudad de México",
+      image: "/public/img/fest-vive-latino.jpg",
+      description: "Durante dos días, la Ciudad de México se convierte en el epicentro de la música latina con actuaciones de artistas nacionales e internacionales, únete a la celebración y disfruta de la energía contagiosa de este icónico festival.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Festival Internacional de la Música y la Danza de Morelia, Michoacán",
+      image: "/public/img/fest-int-danza-morelia.jpg",
+      description: "Este evento anual presenta espectáculos de música clásica, contemporánea y folclórica en algunos de los lugares más emblemáticos de la ciudad, sumérgete en la magia de la música y la danza en este festival único.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Festival Internacional de Jazz de Rivera Maya, Quintana Roo",
+      image: "/public/img/fest-int-rivera-maya.jpg",
+      description: "El Festival Internacional de Jazz de Riviera Maya reúne a renombrados músicos de jazz de todo el mundo para ofrecer conciertos memorables en escenarios impresionantes junto al mar, disfruta de la música, la playa y la buena compañía en este evento inolvidable.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+    {
+      title: "Festival Internacional de Cine de Los Cabos, Baja California Sur",
+      image: "/public/img/fest-int-los-cabos.jpg",
+      description: "Este evento reúne a cineastas, actores y amantes del cine de todo el mundo para celebrar lo mejor del cine independiente y de autor, disfruta de proyecciones al aire libre, eventos especiales y encuentros con la industria cinematográfica en este emocionante festival.",
+      link: "https://www.DreamScape-Detalles.com"
+    },
+  ];
