27b0ab0f26035c42786217989f181ac4da13c8d3
Gustavo Martinez <gutamvol262@gmail.com>
2024-03-12 11:03:21 -0600

    se agrego un js con los datos de las cards

---
+++ src/data/cardsData.js
@@ -0,0 +1,40 @@
+//   arrelo de objestos para la parte de cards
+export const cardsData = [
+    {
+      title: 'Valle de Bravo',
+      image: '/img/valle-de-bravo.jpg',
+      description: 'Un destino que combina la belleza natural con la elegancia colonial. Con su pintoresco lago, sus calles empedradas y sus impresionantes paisajes, Valle de Bravo te invita a disfrutar de actividades al aire libre, relajación y una rica historia cultural.',
+      link: 'https://www.DreamScape-Detalles.com/valle-de-bravo'
+    },
+    {
+      title: 'Tulum, Quintana Roo',
+      image: '/img/tulum-quintana-roo.jpeg',
+      description: 'Sumérgete en la historia en las ruinas arqueológicas, relájate en las impresionantes playas y disfruta de la vibrante escena gastronómica y cultural de este encantador pueblo caribeño.',
+      link: 'https://www.DreamScape-Detalles.com/tulum'
+    },
+    {
+      title: 'San Cristobal de las Casas, Chiapas',
+      image: '/img/san-cristobal-de-las-chiapas.jpg',
+      description: 'Un destino lleno de color, cultura y tradición en el corazón de Chiapas. Con sus calles empedradas, arquitectura colonial y ambiente bohemio, este pueblo te invita a explorar sus mercados artesanales, sitios históricos y paisajes montañosos.',
+      link: 'https://www.DreamScape-Detalles.com/san-cristobal'
+    },
+    {
+      title: 'Mazamitla, Jalisco',
+      image: '/img/mazamitla-jalisco.jpg',
+      description: 'Conocido como la "Suiza mexicana", este destino ofrece paisajes de bosques de pinos, cabañas acogedoras y una atmósfera tranquila que lo convierte en el escape perfecto de la vida urbana. ¡Disfruta de las actividades al aire libre, la gastronomía local y la calidez de su gente!',
+      link: 'https://www.DreamScape-Detalles.com/mazamitla'
+    },
+    {
+      title: 'Real de Catorce, San Luis Potosí',
+      image: '/img/real-de-catorce.jpg',
+      description: 'Un pueblo minero convertido en destino turístico único. Con su arquitectura colonial, calles empedradas y paisajes desérticos, este pueblo te invita a sumergirte en su historia, disfrutar de sus misterios y explorar sus antiguas minas.',
+      link: 'https://www.DreamScape-Detalles.com/real-de-catorce'
+    },
+    {
+      title: 'Bacalar, Quintana Roo',
+      image: '/img/bacalar.jpg',
+      description: 'Sumérgete en aguas cristalinas, explora cenotes secretos y relájate en playas de arena blanca. Bacalar te espera con su belleza natural y su encanto caribeño.',
+      link: 'https://www.DreamScape-Detalles.com/bacalar'
+    }
+  ];
+  
